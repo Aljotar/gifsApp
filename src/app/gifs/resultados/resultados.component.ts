fd4ffb1ed38fd05b563588bbe8773f98d182e6f9
Alvaro Jotar <85590864+Aljotar@users.noreply.github.com>
2023-01-24 00:49:43 -0300

    se crean modulos gifs busqueda resultados

---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styles: [
+    
+  ]
+})
+export class ResultadosComponent {
+
+}
